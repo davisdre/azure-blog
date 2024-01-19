2b82b0a13f9281de6638e93c19061a415f79a34f
Drew Davis <davisdre@hotmail.com>
2024-01-19 01:03:39 +0000

    Remove unnecessary files and dependencies

--- api/src/functions/getConnectionString.js
+++
@@ -1,7 +0,0 @@
-// This is an Azure Function
-module.exports = async function (context, req) {
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: process.env.CONNECTION_STRING
-    };
-}
